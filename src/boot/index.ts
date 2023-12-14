0a1921e6af5e4a054106312ef3d98afe55cbd74f
Ignacio López-Amor <ilopez-amor@ya.com>
2023-12-14 12:30:41 +0100

    feat: add i18n

--- src/boot/index.ts
+++ src/boot/index.ts
@@ -1,15 +1,13 @@
-import { App } from "nativescript-vue";
-import { createPinia } from "pinia";
+import { App } from 'nativescript-vue';
+import { createPinia } from 'pinia';
 
 if (module.hot) {
-  module.hot.accept();
+	module.hot.accept();
 }
 
 export const bootApp = (app: App) => {
-  const pinia = createPinia();
+	const pinia = createPinia();
 
-  app.use(pinia);
-  // app.use(router);
-
-  // app.config.globalProperties.$L = localize;
+	app.use(pinia);
+	// app.use(router);
 };

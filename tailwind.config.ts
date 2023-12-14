f572a4ad73c47bc1997368f9968f5946a6605842
Ignacio López-Amor <ilopez-amor@ya.com>
2023-12-14 12:14:34 +0100

    chore: add linter

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+export default {
+  content: ["./src/**/*.{css,xml,html,vue,svelte,ts,tsx}"],
+  // use the .ns-dark class to control dark mode (applied by NativeScript) - since 'media' (default) is not supported.
+  darkMode: ["class", ".ns-dark"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("android", ".ns-android &");
+      addVariant("ios", ".ns-ios &");
+    },
+  ],
+  corePlugins: {
+    preflight: false, // disables browser-specific resets
+  },
+} satisfies Config;
